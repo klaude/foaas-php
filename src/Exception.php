4094f7a13915febfbb136b71bd8d61eb836187cc
Kevin Laude <nerfyoda@gmail.com>
2015-05-15 01:21:46 -0500

    I demand a more accurate fucking stack trace!
    Bubble the Guzzle exception up into Foaas\Exception.

--- src/Exception.php
+++ src/Exception.php
@@ -10,9 +10,10 @@ class Exception extends \Exception
     /**
      * @param string $message
      * @param int $code
+     * @param \Exception $previous
      */
-    public function __construct($message = '', $code = 0)
+    public function __construct($message = null, $code = 0, \Exception $previous = null)
     {
-        parent::__construct("Oh fuck. {$message}", $code);
+        parent::__construct("Oh fuck. {$message}", $code, $previous);
     }
 }
